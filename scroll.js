6dafcbafd2b82e5308a4dd148de924ad5bc3cdf6
arisgi <m1so.tama16@gmail.com>
2017-06-22 18:32:25 +0900

    Fix

--- scroll.js
+++ scroll.js
@@ -1,5 +1,6 @@
 {
   let vx = vy = 0;
+  let scrolling = false;
 
   document.addEventListener('wheel', (e) => {
     e.preventDefault();
@@ -13,23 +14,27 @@
 
     let ax = ay = 0;
 
-    if (dx > 0) {
+    if (dx >= 0) {
       ax = (dx < ma) ? dx : ma;
     } else {
-      ax = (dx < -ma) ? dx : -ma;
+      ax = (Math.abs(dx) < ma) ? dx : -ma;
     }
 
-    if (dy > 0) {
+    if (dy >= 0) {
       ay = (dy < ma) ? dy : ma;
     } else {
-      ay = (dy < -ma) ? dy : -ma;
+      ay = (Math.abs(dy) < ma) ? dy : -ma;
     }
 
     vx += ax * w;
     vy += ay * w;
 
     window.scrollTo(x + vx, y + vy);
-  })
+
+    if (!scrolling) {
+      vx = vy = 0;
+    }
+  });
 
   document.addEventListener('scroll', (e) => {
     e.preventDefault();
@@ -43,8 +48,11 @@
     const y = window.pageYOffset;
 
     setTimeout(() => {
-      (window.pageXOffset === x && window.pageYOffset === y) && (vx = vy = 0);
-    }, 100)
-
-  })
+      if (window.pageXOffset === x && window.pageYOffset === y) {
+        scrolling = false;
+      } else {
+        scrolling = true;
+      }
+    }, 100);
+  });
 }
